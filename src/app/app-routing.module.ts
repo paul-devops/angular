0e2f7ccccae791d53a99d44d689dec3782ad5d23
paul-devops <paul.irie@operantic.com>
2020-01-05 13:22:58 +0100

    First commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {AppComponent} from './app.component';
+import {ContactComponent} from './contact/contact.component';
+import {InscriptionComponent} from './inscription/inscription.component';
+import {ConnexionComponent} from './connexion/connexion.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'inscription', component: InscriptionComponent},
+  {path: 'connexion', component: ConnexionComponent},
+  {path: 'contact', component: ContactComponent}
+  ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ConnexionComponent } from './connexion/connexion.component';
 import { InscriptionComponent } from './inscription/inscription.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ConnexionComponent,
-    InscriptionComponent
+    InscriptionComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
